e99a3f9074f68abd2e7f4765bab9c396ead19741
SrGalleta444 <cnm.arielanaya@gmail.com>
2025-04-10 11:43:41 -0600

    Create Ejercicio1 .c

---
+++ Ejercicio1 .c
@@ -0,0 +1,15 @@
+// Practica 8 Ariel Anaya
+// Ejercicio 1
+// numeros del 1 al 100
+#include <stdio.h>
+
+void main()
+{
+   int n = 1;
+   
+   for(n = 1; n <= 100; n++){
+       printf("%d \n", n);
+       
+   }
+   
+}
